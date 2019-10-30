a72e390c439eb12bd0ba37f38b4d672b3ba25a1c
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-10-30 13:30:22 +0200

    Add Access-Control Headers

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,28 @@ const key = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(key),
-  databaseURL: 'https://tazkrtak.firebaseio.com'
+  databaseURL: "https://tazkrtak.firebaseio.com"
 });
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
+const cors = {
+  origin: ["https://tazkrtak-admin.web.app", "http://127.0.0.1:4000"]
+};
+
+app.use((req, res, next) => {
+  let origin = req.headers.origin;
+  if (cors.origin.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(bodyParser.json());
 
