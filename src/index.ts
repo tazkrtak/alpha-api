18611cd0149c391eaffc81928a09a1be2979920b
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-11-21 20:50:15 +0200

    Fix CROS Origin Bug

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,22 @@ admin.initializeApp({
   databaseURL: "https://tazkrtak.firebaseio.com"
 });
 
-
 /* Express */
 
-import express = require("express");
-import bodyParser = require("body-parser");
+import express     = require("express");
+import bodyParser  = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+/* Routers */
 
 import rootRouter  = require("./routes/root");
 import busesRouter = require("./routes/buses");
@@ -21,32 +32,10 @@ import staffRouter = require("./routes/staff");
 import usersRouter = require("./routes/users");
 import transRouter = require("./routes/transactions");
 
-const app = express();
-
-app.use(bodyParser.json());
-
 app.use("/", rootRouter);
 app.use("/buses", busesRouter);
 app.use("/staff", staffRouter);
 app.use("/users", usersRouter);
 app.use("/transactions", transRouter);
-
-
-const cors = {
-  origin: ["https://tazkrtak-admin.web.app", "http://127.0.0.1:4000"]
-};
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin || ' ';
-  if (cors.origin.includes(origin[0])) {
-    res.header("Access-Control-Allow-Origin", origin[0]);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 
 exports.api = functions.https.onRequest(app);
